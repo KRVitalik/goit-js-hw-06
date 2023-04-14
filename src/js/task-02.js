5c5c9e21f76b16fafd6b5ebe9d2ca044a6f2ab11
KRVitalik <karmelitavitalik@gmail.com>
2023-04-14 14:23:02 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const addNewIngredients = document.querySelector('ul#ingredients');
+
+const element = ingredients.map((ingredient) => {
+  const createIngredients = document.createElement('li');
+  createIngredients.textContent = `${ingredient}`;
+  createIngredients.classList.add('item');
+
+  return createIngredients
+}); 
+  addNewIngredients.append(...element);
