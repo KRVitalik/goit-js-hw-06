273dde4218900063aa4748d7d1532d7735d32ae7
KRVitalik <karmelitavitalik@gmail.com>
2023-04-21 14:20:36 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const registerForm = document.querySelector(".login-form");
+const emptyEmail = 'Email is empty';
+const emptyPassword = 'Password is empty';
+
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const { email, password } = event.currentTarget;
+
+  if (email.value === '') { window.alert(emptyEmail) }
+  else if (password.value === '') { window.alert(emptyPassword) }
+  
+  const elements = {
+    email: email.value,
+    password: password.value,
+  }
+  document.querySelector('.login-form').reset();
+  console.log(elements);
+});
 
