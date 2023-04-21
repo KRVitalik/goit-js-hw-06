07946b5c6452a4510f10743bc76b6b96b378bbe3
KRVitalik <karmelitavitalik@gmail.com>
2023-04-21 14:49:43 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const randomBackGroundColor = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const colorRandomName = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', () => {
+  randomBackGroundColor.style.backgroundColor = getRandomHexColor();
+  colorRandomName.textContent = `- ${getRandomHexColor()}`;
+  // btnChangeColor.style.backgroundColor = getRandomHexColor();
+  // colorRandomName.style.color = btnChangeColor.style.backgroundColor;
+  // btnChangeColor.style.color = randomBackGroundColor.style.backgroundColor;
+});
