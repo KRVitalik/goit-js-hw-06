3e8cede3f8f77a6e29faf9705d1306534511b07c
KRVitalik <karmelitavitalik@gmail.com>
2023-04-26 14:41:33 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divBoxes = document.querySelector('#boxes');
+const elementCount = document.querySelector('[type="number"]');
+const buttonCreateCollections = document.querySelector('[data-create]');
+const buttonDestroyCollection = document.querySelector('[data-destroy]');
+
+let newElementWidthHeight = 30;
+
+buttonCreateCollections.addEventListener('click', () => {
+  const amount = Number(elementCount.value);
+  createBoxes(amount);
+});
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const divCollections = document.createElement('div');
+    divCollections.style.width = `${newElementWidthHeight}px`;
+    divCollections.style.height = `${newElementWidthHeight}px`;
+    divCollections.style.backgroundColor = getRandomHexColor();
+    divBoxes.appendChild(divCollections);
+    newElementWidthHeight += 10;
+    divBoxes.style.marginTop = `${5}px`
+  }
+}
+
+buttonDestroyCollection.addEventListener('click', () => {
+  destroyBoxes();
+});
+
+function destroyBoxes() {
+  divBoxes.innerHTML = '';
+  newElementWidthHeight = 0;
+}
+
+
+
